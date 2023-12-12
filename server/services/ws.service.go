41158be667bef9b610a1084531ead91074be882a
RolandWarburton <warburtonroland@gmail.com>
2023-12-13 08:37:04 +1100

    Handle different websocket messages test

--- server/services/ws.service.go
+++ server/services/ws.service.go
@@ -13,8 +13,14 @@ func ListenToWSEvents(listener chan string) {
 	fmt.Println("listening to events")
 	// Start a goroutine to listen for events
 	go func() {
+		// iterate over each value on the listener as they arrive
 		for data := range listener {
-			fmt.Println("Received message:", data)
+			switch data {
+			case "ping":
+				fmt.Println("pong")
+			default:
+				fmt.Println("idk")
+			}
 		}
 	}()
 }
